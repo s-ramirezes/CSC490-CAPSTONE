0a7e18737f8ad4287602576a18e20e9e50eeb75e
s-ramirezes <s_ramirezes@uncg.edu>
2024-09-10 15:48:53 -0400

    updated paths

--- Prototype/app.js
+++ Prototype/app.js
@@ -5,8 +5,6 @@ const app = express();
 const multer = require("multer");
 const session = require("express-session");
 const path = require("path");
-const passport = require('passport');
-require("./auth/passport");
 
 // const imageStorage = multer.diskStorage({
 //   destination: function (req, file, cb) {
@@ -43,9 +41,6 @@ app.use(session({
   saveUninitialized: false
 }));
 
-app.use(passport.initialize());
-app.use(passport.session());
-app.use('/auth', require('./auth/auth.route'));
 
 const moderatorRouter = require("./routes/moderator.route");
 const userRouter = require("./routes/user.route");
@@ -55,10 +50,10 @@ const loginRouter = require("./routes/login.route");
 
 app.use("/public", express.static('public'));
 app.use("/images", express.static('images'));
-app.use("/", moderatorRouter);
-app.use("/", userRouter);
-app.use("/", teacherRouter);
-app.use("/", tutorRouter);
+app.use("/mod", moderatorRouter);
+app.use("/user", userRouter);
+app.use("/teacher", teacherRouter);
+app.use("/tutor", tutorRouter);
 app.use("/", loginRouter);
 
 const PORT = process.env.PORT || 8000;
