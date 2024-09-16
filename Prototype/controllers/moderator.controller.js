844de01343e64294d697e7f779c18ec7d02d96cf
SebastianDuarteG <Sdduartegua@uncg.edu>
2024-09-16 02:19:49 -0400

    Messaging implemented

--- Prototype/controllers/moderator.controller.js
+++ Prototype/controllers/moderator.controller.js
@@ -1,5 +1,4 @@
 "use strict";
-
 const express = require("express");
 const app = express();
 // const path = require('path')
@@ -7,9 +6,7 @@ const multer = require("multer");
 // app.use(multer().none());
 // app.use(express.urlencoded({ extended: true }));
 // app.use(express.json());
-
 const model = require("../models/moderator.model");
-
 function homePage (req,res) {
     try {
         const subjects = model.getSubjects();
@@ -20,22 +17,42 @@ function homePage (req,res) {
         console.error("Failed to render modHome page: "+ err.message);
     }
 }
-
 function messages (req, res){
     try{
         const subjects= model.getSubjects();
         req.session.subjects = subjects;
+        const messages= model.getMessages(req.query.convId);
+        const userId= 2;
+        const receiver= model.getReceiver(userId);
+        const convId = req.query.convId; 
         const role= 'moderator';
-        res.render ("modMessages", {subjects : subjects, role: role});
+        res.render ("modMessages", {subjects : subjects, role: role, 
+            messages: messages, userId: userId, receiver: receiver, 
+            convId: convId});
     } catch (err) {
         console.error("Failed to render modMessages page "+ err.message);
     }
 }
+function sendMessage (req, res){
+    try{
+        const userId = req.body.userId;
+        const messageTxt = req.body.message;
+        const convId = req.query.convId;
+        if (!convId || !userId || !messageTxt) {
+            throw new Error("Missing required parameters");
+        }
+        model.storeMessage(convId, messageTxt, userId);
 
 
-
+        res.redirect(`/modMessages?convId=${convId}`);
+    } catch (err) {
+        console.error ("Failed to send Message: " + err.message);
+        res.status(500).send("Failed to send message");
+    }
+}
 module.exports = {
     homePage,
     messages,
-    
+    sendMessage,
+
 };
--- Prototype/models/moderator.model.js
+++ Prototype/models/moderator.model.js
@@ -1,12 +1,40 @@
 "use strict";
 const db = require("../models/db-conn");
-
 function getSubjects (){
     const sql = "SELECT * FROM category";
     return db.all(sql);
 }
 
+function getMessages (convId){
+    const sql = `SELECT * FROM messages
+        WHERE convId= ?
+        ORDER BY messageTime ASC;`;
+    return db.all(sql, convId);
+}
+
+function getReceiver(userId){
+    const sql = `SELECT u.fName, u.lname FROM conversation c
+        JOIN users u
+        ON u.userId = 
+            CASE 
+            WHEN c.userId1 = ? THEN c.userId2
+            ELSE c.userId1
+            END
+        WHERE c.userId1 = ? OR c.userId2 = ?;`;
+    return db.get(sql, userId, userId, userId);
+}
+
+function storeMessage(convId, messageTxt, userId) {
+    console.log(convId, messageTxt, userId);
+    const sql = `INSERT INTO messages (convId, description, userId)
+        VALUES (?,?,?);`;
+    return db.run(sql, [convId, messageTxt, userId]);
+}
+
 module.exports = {
     getSubjects,
-    
+    getMessages,
+    getReceiver,
+    storeMessage,
+
 };
