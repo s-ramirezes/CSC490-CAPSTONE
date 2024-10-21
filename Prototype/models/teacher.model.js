b6a1e4c6b98d75897fa1aeaadd10fe829f1fa366
s-ramirezes <s_ramirezes@uncg.edu>
2024-10-20 20:22:42 -0400

    added more filter options to analytics page

--- Prototype/models/teacher.model.js
+++ Prototype/models/teacher.model.js
@@ -28,19 +28,32 @@ function promoteToTutor(userId) {
 }
 
 
-function getLeaderboard(catName, filterStartDate, filterEndDate) {
+function getLeaderboard(catName, filterStartDate, filterEndDate, filterCourse, filterUser, filterTitle) {
     let sql = `
     SELECT u.userId, u.profilePic, u.fname, u.lname, u.role, COUNT(p.postId) AS postCount
     FROM users u
     JOIN posts p ON u.userId = p.userId
     JOIN category c ON p.catId = c.catId
-    WHERE c.catName = ? AND u.role != 'teacher'`;
+    WHERE c.catName = ? AND u.role NOT IN ('teacher', 'moderator')`;
 
     const params = [catName];
 
     if (filterStartDate || filterEndDate) {
         sql += ` AND p.postTime BETWEEN ? AND ?`;
         params.push(filterStartDate, filterEndDate);
+    }
+    if (filterCourse && filterCourse !== 'All' ) {
+        sql += ` AND p.courseId = ?`;
+        params.push(filterCourse);
+    }
+    if (filterUser && filterUser !== 'All') {
+        sql += ` AND p.userId = ?`;
+        params.push(filterUser);
+    }
+
+    if (filterTitle && filterTitle !== 'All') {
+        sql += ` AND p.title = ?`;
+        params.push(filterTitle);
     }
 
     sql += `ORDER BY postCount DESC
@@ -49,14 +62,15 @@ function getLeaderboard(catName, filterStartDate, filterEndDate) {
     return db.all(sql, ...params);
 }
 
-function getAmountofPosts(catName, filterStartDate, filterEndDate) {
+function getAmountofPosts(catName, filterStartDate, filterEndDate, filterCourse, filterUser, filterTitle) {
     let sql = `
     SELECT 
     p.courseId, 
         COUNT(p.postId) AS postCount
     FROM posts p
     JOIN category c ON p.catId = c.catId
-    WHERE c.catName = ?`;
+    JOIN users u ON p.userId = u.userId
+    WHERE c.catName = ? AND u.role NOT IN ('teacher', 'moderator')`;
 
     const params = [catName];
 
@@ -64,19 +78,32 @@ function getAmountofPosts(catName, filterStartDate, filterEndDate) {
         sql += ` AND p.postTime BETWEEN ? AND ?`;
         params.push(filterStartDate, filterEndDate);
     }
+    if (filterCourse && filterCourse !== 'All' ) {
+        sql += ` AND p.courseId = ?`;
+        params.push(filterCourse);
+    }
+    if (filterUser && filterUser !== 'All') {
+        sql += ` AND p.userId = ?`;
+        params.push(filterUser);
+    }
+
+    if (filterTitle && filterTitle !== 'All') {
+        sql += ` AND p.title = ?`;
+        params.push(filterTitle);
+    }
 
     sql += ` GROUP BY p.courseId`;
 
     return db.all(sql, ...params);
 }
 
-function getPosts(catName, filterStartDate, filterEndDate, filterCourse, filterUser) {
+function getPosts(catName, filterStartDate, filterEndDate, filterCourse, filterUser, filterTitle) {
     let sql = `
-    SELECT p.*, u.email, u.profilePic, u.fname, u.lname, u.userId, c.catAbbr
+    SELECT p.*, u.email, u.profilePic, u.fname, u.lname, u.userId, u.role, c.catAbbr
     FROM posts p
     JOIN category c ON p.catId = c.catId
     JOIN users u ON p.userId = u.userId
-    WHERE c.catName = ?`;
+    WHERE c.catName = ? AND u.role NOT IN ('teacher', 'moderator')`;
     
     const params = [catName];
 
@@ -93,15 +120,20 @@ function getPosts(catName, filterStartDate, filterEndDate, filterCourse, filterU
         params.push(filterUser);
     }
 
+    if (filterTitle && filterTitle !== 'All') {
+        sql += ` AND p.title = ?`;
+        params.push(filterTitle);
+    }
+
     return db.all(sql, ...params);
 }
 
-function getReviewLeaderboard(catName, filterStartDate, filterEndDate) {
+function getReviewLeaderboard(catName, filterStartDate, filterEndDate, filterCourse, filterUser, reviewFilterTitle) {
     let sql = `
     SELECT u.userId, u.profilePic, u.fname, u.lname, u.role, COUNT(r.reviewId) AS reviewCount
     FROM users u
     JOIN reviews r ON u.userId = r.revieweeId
-    WHERE r.subject = ? AND u.role != 'teacher'`;
+    WHERE r.subject = ? AND u.role NOT IN ('teacher', 'moderator')`;
 
     const params = [catName];
 
@@ -109,25 +141,50 @@ function getReviewLeaderboard(catName, filterStartDate, filterEndDate) {
         sql += ` AND r.reviewTime BETWEEN ? AND ?`;
         params.push(filterStartDate, filterEndDate);
     }
+    if (filterCourse && filterCourse !== 'All' ) {
+        sql += ` AND r.courseId = ?`;
+        params.push(filterCourse);
+    }
+    if (filterUser && filterUser !== 'All') {
+        sql += ` AND r.revieweeId = ?`;
+        params.push(filterUser);
+    }
+    if (reviewFilterTitle && reviewFilterTitle !== 'All') {
+        sql += ` AND r.title = ?`;
+        params.push(reviewFilterTitle);
+    }
 
     sql += ` ORDER BY reviewCount DESC LIMIT 10`;
 
     return db.all(sql, ...params);
 }
 
-function getAmountofReviews(catName, filterStartDate, filterEndDate) {
+function getAmountofReviews(catName, filterStartDate, filterEndDate, filterCourse, filterUser, reviewFilterTitle) {
     let sql = `
     SELECT 
         r.courseId, 
         COUNT(r.reviewId) AS reviewCount
     FROM reviews r
-    WHERE r.subject = ?`;
+    JOIN users u ON r.revieweeId = u.userId
+    WHERE r.subject = ? AND u.role NOT IN ('teacher', 'moderator')`;
 
     const params = [catName];
 
     if (filterStartDate || filterEndDate) {
         sql += ` AND r.reviewTime BETWEEN ? AND ?`;
         params.push(filterStartDate, filterEndDate);
+    }
+    if (filterCourse && filterCourse !== 'All' ) {
+        sql += ` AND r.courseId = ?`;
+        params.push(filterCourse);
+    }
+    if (filterUser && filterUser !== 'All') {
+        sql += ` AND r.revieweeId = ?`;
+        params.push(filterUser);
+    }
+    if (reviewFilterTitle && reviewFilterTitle !== 'All') {
+        sql += ` AND r.title = ?`;
+        params.push(reviewFilterTitle);
     }
 
     sql += ` GROUP BY r.courseId`;
@@ -136,12 +193,13 @@ function getAmountofReviews(catName, filterStartDate, filterEndDate) {
 }
 
 
-function getReviews(catName, filterStartDate, filterEndDate, filterCourse, filterUser){
+function getReviews(catName, filterStartDate, filterEndDate, filterCourse, filterUser, reviewFilterTitle){
     let sql = `
-    SELECT r.*, u.email, u.profilePic, u.fname, u.lname, u.userId
+    SELECT r.*, u.email, u.profilePic, u.fname, u.lname, u.userId, u.role, c.catAbbr
     FROM reviews r
     JOIN users u ON r.revieweeId = u.userId
-    WHERE r.subject = ?`;
+    JOIN category c ON r.subject = c.catName
+    WHERE r.subject = ? AND u.role NOT IN ('teacher', 'moderator') `;
     
     const params = [catName];
 
@@ -156,6 +214,10 @@ function getReviews(catName, filterStartDate, filterEndDate, filterCourse, filte
     if (filterUser && filterUser !== 'All') {
         sql += ` AND r.revieweeId = ?`;
         params.push(filterUser);
+    }
+    if (reviewFilterTitle && reviewFilterTitle !== 'All') {
+        sql += ` AND r.title = ?`;
+        params.push(reviewFilterTitle);
     }
 
     return db.all(sql, ...params);
