1babcdbeb49017f3f8a0e446ab12e79a61e9db38
SebastianDuarteG <Sdduartegua@uncg.edu>
2024-10-20 21:19:23 -0400

    merge

--- Prototype/models/user.model.js
+++ Prototype/models/user.model.js
@@ -18,7 +18,7 @@ function getSubjectPosts(catId) {
 }
 
 function getAllSubjects(){
-    const sql = 'SELECT * FROM category';
+    const sql = "SELECT * FROM category ORDER BY catAbbr ASC";
     return db.all(sql);
 }
 
